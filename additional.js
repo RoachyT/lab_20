a9460c132b7c4833d64d811391428f9c1a4950c5
Rochelle Toles <Rochelletoles@quickenloans.com>
2018-11-15 09:06:31 -0500

    pushing

---
+++ additional.js
@@ -0,0 +1,12 @@
+$(document).ready();
+window.onscroll = function(){myfunction()};
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+function myfunction(){
+    if(window.pageYOffset > sticky){
+        header.classList.add('sticky');
+    }
+    else{
+        header.classList.remove("sticky");
+    }
+}
